1b1a8c8213ed64bc2b3adfacef208c47f7355a26
Marcelo Galli <marcelolemesgalli@hotmail.com>
2023-08-27 22:51:22 -0300

    configurando testes

--- README.md
+++ README.md
@@ -163,3 +163,4 @@
     ```
 
 # Testes
+- pip install pytest pytest-django
--- recipes/tests.py
+++ recipes/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+# todo arquivo que começar com test_* será executado como test no django
+class RecipeURLsTest(TestCase):
+    def test_the_pytest_is_ok(self):
+        ...
